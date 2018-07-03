1acf2f8fb7397bfd34e79c5c1531eaf52cca865c
maksimkirillov <max.kirillov@mail.ru>
2018-07-04 01:03:47 +0700

    calculator(switch)

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -28,54 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.addition = new System.Windows.Forms.Button();
+            this.sabtraction = new System.Windows.Forms.Button();
+            this.multyplication = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // addition
             // 
-            this.button1.Location = new System.Drawing.Point(12, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Сложение";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.addition.Location = new System.Drawing.Point(12, 85);
+            this.addition.Name = "addition";
+            this.addition.Size = new System.Drawing.Size(75, 23);
+            this.addition.TabIndex = 0;
+            this.addition.Text = "Сложение";
+            this.addition.UseVisualStyleBackColor = true;
+            this.addition.Click += new System.EventHandler(this.Click);
             // 
-            // button2
+            // sabtraction
             // 
-            this.button2.Location = new System.Drawing.Point(191, 85);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Вычитание ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.sabtraction.Location = new System.Drawing.Point(191, 85);
+            this.sabtraction.Name = "sabtraction";
+            this.sabtraction.Size = new System.Drawing.Size(75, 23);
+            this.sabtraction.TabIndex = 1;
+            this.sabtraction.Text = "Вычитание ";
+            this.sabtraction.UseVisualStyleBackColor = true;
+            this.sabtraction.Click += new System.EventHandler(this.Click);
             // 
-            // button3
+            // multyplication
             // 
-            this.button3.Location = new System.Drawing.Point(13, 170);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Умножение";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.multyplication.Location = new System.Drawing.Point(13, 170);
+            this.multyplication.Name = "multyplication";
+            this.multyplication.Size = new System.Drawing.Size(75, 23);
+            this.multyplication.TabIndex = 2;
+            this.multyplication.Text = "Умножение";
+            this.multyplication.UseVisualStyleBackColor = true;
+            this.multyplication.Click += new System.EventHandler(this.Click);
             // 
-            // button4
+            // division
             // 
-            this.button4.Location = new System.Drawing.Point(191, 170);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Деление";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.division.Location = new System.Drawing.Point(191, 170);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(75, 23);
+            this.division.TabIndex = 3;
+            this.division.Text = "Деление";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.Click);
             // 
             // textBox1
             // 
@@ -106,10 +106,10 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.multyplication);
+            this.Controls.Add(this.sabtraction);
+            this.Controls.Add(this.addition);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -119,10 +119,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button addition;
+        private System.Windows.Forms.Button sabtraction;
+        private System.Windows.Forms.Button multyplication;
+        private System.Windows.Forms.Button division;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
