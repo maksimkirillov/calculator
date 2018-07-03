4a62153d35602eb22703beae571f3f2c156a7a60
maksimkirillov <max.kirillov@mail.ru>
2018-07-04 00:56:13 +0700

    simple calculator

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -55,6 +55,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Вычитание ";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -64,6 +65,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Умножение";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -73,6 +75,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Деление";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // textBox1
             // 
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -25,5 +25,35 @@ namespace Calculator
             double result = secondValue + firstValue;
             textBox3.Text = result.ToString();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string firstValuetext = textBox1.Text;
+            double firstValue = Convert.ToDouble(firstValuetext);
+            string secondValuetext = textBox2.Text;
+            double secondValue = Convert.ToDouble(secondValuetext);
+            double result = secondValue - firstValue;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string firstValuetext = textBox1.Text;
+            double firstValue = Convert.ToDouble(firstValuetext);
+            string secondValuetext = textBox2.Text;
+            double secondValue = Convert.ToDouble(secondValuetext);
+            double result = secondValue * firstValue;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string firstValuetext = textBox1.Text;
+            double firstValue = Convert.ToDouble(firstValuetext);
+            string secondValuetext = textBox2.Text;
+            double secondValue = Convert.ToDouble(secondValuetext);
+            double result = secondValue / firstValue;
+            textBox3.Text = result.ToString();
+        }
     }
 }
