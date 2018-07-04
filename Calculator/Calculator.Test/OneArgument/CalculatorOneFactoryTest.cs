7808a9ca8e88356d2f452772e7e2ea62c15c291a
maksimkirillov <max.kirillov@mail.ru>
2018-07-05 00:49:53 +0700

    TestOneFactoryArgument

---
+++ Calculator/Calculator.Test/OneArgument/CalculatorOneFactoryTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Calculator.Test
+{
+    public class CalculatorOneFactoryTest
+    {
+        [TestCase("Cos", typeof(Cos))]
+        [TestCase("Sin", typeof(Sin))]
+        [TestCase("Sqrt", typeof(Sqrt))]
+        [TestCase("Abs", typeof(Abs))]
+        [TestCase("Log", typeof(Log))]
+        [TestCase("Log10", typeof(Log10))]
+        [TestCase("Tg", typeof(Tg))]
+        [TestCase("Square", typeof(Square))]
+        [TestCase("Cube", typeof(Cube))]
+        public void CalculattorTest(string name, Type type)
+        {
+            var calculator = CalculatorOneFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+
+}
