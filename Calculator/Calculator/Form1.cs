2caedbb77adcc33b5937f7b3a25272c77cd88985
maksimkirillov <max.kirillov@mail.ru>
2018-07-05 23:28:38 +0700

    addition try and catch

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -11,26 +11,37 @@ namespace Calculator
         }
         private void Click(object sender, EventArgs e)
         {
-            string firstValuetext = textBox1.Text;
-            double firstValue = Convert.ToDouble(firstValuetext);
-            string secondValuetext = textBox2.Text;
-            double secondValue = Convert.ToDouble(secondValuetext);
-
-
-            string caluclateName = ((Button)sender).Name;
-
-            ICalculateTwoArguments calculator = CalculatorTwoFactory.CreateCalculator(caluclateName);
-            double result = calculator.Calculator(firstValue, secondValue);
-            textBox3.Text = result.ToString();
+            try
+            {
+                string firstValuetext = textBox1.Text;
+                double firstValue = Convert.ToDouble(firstValuetext);
+                string secondValuetext = textBox2.Text;
+                double secondValue = Convert.ToDouble(secondValuetext);
+                string caluclateName = ((Button) sender).Name;
+                ICalculateTwoArguments calculator = CalculatorTwoFactory.CreateCalculator(caluclateName);
+                double result = calculator.Calculator(firstValue, secondValue);
+                textBox3.Text = result.ToString();
+            }
+            catch(Exception TwoArgumentException)
+            {
+                MessageBox.Show("Неверное выражение" + TwoArgumentException);
+            }
         }
         private void Click1(object sender, EventArgs e)
         {
-            string firstValuetext = textBox1.Text;
-            double firstValue = Convert.ToDouble(firstValuetext);
-            string caluclateName = ((Button)sender).Name;
-            ICalculatorOneArgument calculator1 = CalculatorOneFactory.CreateCalculator(caluclateName);
-            double result = calculator1.Calculator(firstValue);
-            textBox3.Text = result.ToString();
+            try
+            {
+                string firstValuetext = textBox1.Text;
+                double firstValue = Convert.ToDouble(firstValuetext);
+                string caluclateName = ((Button) sender).Name;
+                ICalculatorOneArgument calculator1 = CalculatorOneFactory.CreateCalculator(caluclateName);
+                double result = calculator1.Calculator(firstValue);
+                textBox3.Text = result.ToString();
+            }
+            catch (Exception OneArgumentException)
+            {
+                MessageBox.Show("Неверное выражение" + OneArgumentException);
+            }
         } 
     }
 }
